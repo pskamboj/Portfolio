2ae931113bba4687d359917b7f6e1c13b41005c0
pskamboj <112372068+pskamboj@users.noreply.github.com>
2024-07-03 01:42:25 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,84 @@
+import logo from "./logo.svg";
+import chatbot from "./chatbot.jpeg";
+import self_driving from "./self_driving.png";
+import analytics from "./analytics.png";
+import computer_vision from "./computer vision.jpeg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import tensorflow from "./tech/tensorflow.png";
+
+import css from "./tech/css.png";
+import yolo from "./tech/yolo.png";
+import opencv from "./tech/opencv.png";
+import pytorch from "./tech/pytorch.png"
+import huggingFace from "./tech/hugging.svg"
+import keras from "./tech/keras.png"
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import python from "./tech/python.png"
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  chatbot,
+  self_driving,
+  analytics,
+  computer_vision,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  opencv,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  python,
+  tensorflow,
+  typescript,
+  keras,
+  pytorch,
+  huggingFace,
+  yolo,
+};
